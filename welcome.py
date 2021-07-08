55fb4a5b931339175d48959405a55e0876696570
ssebastianoo <sebastianogirotto05@gmail.com>
2021-07-08 08:49:22 +0200

    removed username check

--- welcome.py
+++ welcome.py
@@ -80,7 +80,8 @@ def on_chat_message(msg):
 
         for member in members:
             if member["id"] != bot.getMe()["id"]:
-                if str(member['username']).lower() not in users:
+
+                """if str(member['username']).lower() not in users:
                     if str(member['username']).lower() == config.bot.bot_nick:
                         return
                     bot.kickChatMember(chat_id, member['id'])
@@ -88,12 +89,14 @@ def on_chat_message(msg):
                     return
                 else:
                     if int(msg["chat"]["id"]) != int(users[str(member["username"]).lower()]["group"]):
-                        return bot.kickChatMember(chat_id, member['id'])
-                    m = bot.sendVideo(chat_id, config.groups.welcome[msg["chat"]["id"]], caption = f"@{member['username']} è entrato/a", reply_to_message_id = msg['message_id'])
-                    now = datetime.datetime.utcnow()
-                    then = (now + datetime.timedelta(seconds=80))
-                    raw  = f"{then.day if len(str(then.day)) != 1 else f'0{then.day}'}/{then.month if len(str(then.month)) != 1 else f'0{then.month}'}/{then.year if len(str(then.year)) != 1 else f'0{then.year}'} {then.hour if len(str(then.hour)) != 1 else f'0{then.hour}'}:{then.minute if len(str(then.minute)) != 1 else f'0{then.minute}'}:{then.second if len(str(then.second)) != 1 else f'0{then.second}'}"
-                    cursor.execute("INSERT INTO welcome (time, chat_id, message_id) VALUES (%s, %s, %s)", (raw, chat_id, m["message_id"]))
+                        return bot.kickChatMember(chat_id, member['id'])"""
+
+                m = bot.sendVideo(chat_id, config.groups.welcome[msg["chat"]["id"]], caption = f"@{member['username']} è entrato/a", reply_to_message_id = msg['message_id'])
+                now = datetime.datetime.utcnow()
+                then = (now + datetime.timedelta(seconds=80))
+                raw  = f"{then.day if len(str(then.day)) != 1 else f'0{then.day}'}/{then.month if len(str(then.month)) != 1 else f'0{then.month}'}/{then.year if len(str(then.year)) != 1 else f'0{then.year}'} {then.hour if len(str(then.hour)) != 1 else f'0{then.hour}'}:{then.minute if len(str(then.minute)) != 1 else f'0{then.minute}'}:{then.second if len(str(then.second)) != 1 else f'0{then.second}'}"
+                cursor.execute("INSERT INTO welcome (time, chat_id, message_id) VALUES (%s, %s, %s)", (raw, chat_id, m["message_id"]))
+                
         db.commit()
         db.close()
 
